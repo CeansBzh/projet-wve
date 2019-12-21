ec8ca137ed6dcbf70d80f037c8b112d3ff47a572
Ceansbzh <ceansbzh@gmail.com>
2019-12-21 18:10:22 +0100

    Créations des bases du système de compte du projet "Voyages"

---
+++ inscription.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+include('connexionDB.php');
+
+if(isset($_POST['forminscription'])) {
+  $pseudo = htmlspecialchars($_POST['pseudo']);
+  $mail = htmlspecialchars($_POST['mail']);
+  $mail2 = htmlspecialchars($_POST['mail2']);
+  $mdp = htmlspecialchars($_POST['mdp']);
+  $mdp2 = htmlspecialchars($_POST['mdp2']);
+  if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
+    $valid = true;
+    $pseudolength = strlen($pseudo);
+    if($pseudolength <= 255) {
+     if($mail == $mail2) {
+       if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+         // On vérifit que le mail est disponible
+         $req_mail = $DB->query("SELECT email FROM users WHERE email = ?", array($mail));
+         $req_mail = $req_mail->fetch();
+         if ($req_mail['email'] <> ""){
+           $valid = false;
+           $er_mail = "Ce mail existe déjà";
+         }
+          if($valid != false) {
+           if($mdp == $mdp2) {
+             $mdp = crypt($mdp, '$6$rounds=5000$szgrzgerggegehrhhfshsh156s1@tfhs6h146-6GRS6G4¨^drfg4dg$');
+             $date_inscription = date('Y-m-d H:i:s');
+             // bin2hex(random_bytes($length))
+             $token = bin2hex(random_bytes(12));
+             // Exemples:
+             // 39e9289a5b8328ecc4286da11076748716c41ec7fb94839a689f7dac5cdf5ba8bdc9a9acdc95b95245f80a00
+             // On insert nos données dans la table utilisateur
+             $DB->insert("INSERT INTO users(username, email, password, date_inscritption, token) VALUES(?, ?, ?, ?, ?)", array($pseudo, $mail, $mdp, $date_inscription, $token));
+             $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
+             //Envoi d'un email de confirmation de compte
+             $req = $DB->query("SELECT * FROM utilisateur WHERE mail = ?", array($mail));
+             $req = $req->fetch();
+             $mail_to = $req['mail'];
+             //Création du header de l'e-mail.
+             $header = "From: no-reply@gmail.com\n";
+             $header .= "MIME-version: 1.0\n";
+             $header .= "Content-type: text/html; charset=utf-8\n";
+             $header .= "Content-Transfer-ncoding: 8bit";
+             //Fin header email
+             //Ajout du message au format HTML          
+             $contenu = '<p>Bonjour ' . $req['nom'] . ',</p><br>
+             <p>Veuillez confirmer votre compte <a href="http://localhost/projet-wve/conf?id=' . $req['id'] . '&token=' . $token . '">Valider</a><p>';     
+             mail($mail_to, 'Confirmation de votre compte', $contenu, $header);
+           } else {
+             $erreur = "Vos mots de passes ne correspondent pas !";
+           }
+         } else {
+           $erreur = "Adresse mail déjà utilisée !";
+         }
+       } else {
+         $erreur = "Votre adresse mail n'est pas valide !";
+       }
+     } else {
+       $erreur = "Vos adresses mail ne correspondent pas !";
+     }
+   } else {
+     $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+   }
+ } else {
+   $erreur = "Tous les champs doivent être complétés !";
+ }
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Inscription</title>
+    </head>
+    <body>      
+        <div>Inscription</div>
+        <form method="POST" action="">
+            <table>
+               <tr>
+                  <td align="right">
+                     <label for="pseudo">Pseudo :</label>
+                  </td>
+                  <td>
+                     <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" />
+                  </td>
+               </tr>
+               <tr>
+                  <td align="right">
+                     <label for="mail">Mail :</label>
+                  </td>
+                  <td>
+                     <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
+                  </td>
+               </tr>
+               <tr>
+                  <td align="right">
+                     <label for="mail2">Confirmation du mail :</label>
+                  </td>
+                  <td>
+                     <input type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
+                  </td>
+               </tr>
+               <tr>
+                  <td align="right">
+                     <label for="mdp">Mot de passe :</label>
+                  </td>
+                  <td>
+                     <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp" />
+                  </td>
+               </tr>
+               <tr>
+                  <td align="right">
+                     <label for="mdp2">Confirmation du mot de passe :</label>
+                  </td>
+                  <td>
+                     <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" />
+                  </td>
+               </tr>
+               <tr>
+                  <td></td>
+                  <td align="center">
+                     <br />
+                     <input type="submit" name="forminscription" value="Je m'inscris" />
+                  </td>
+               </tr>
+            </table>
+         </form>
+         <?php
+         if(isset($erreur)) {
+            echo '<font color="red">'.$erreur."</font>";
+         }
+         ?>
+    </body>
+</html>
