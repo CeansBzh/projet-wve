d53b57ae786190d5fe861b2d437e24d1c7b7adce
Ceansbzh <ceansbzh@gmail.com>
2019-12-28 13:50:19 +0100

    test

--- inscription.php
+++ inscription.php
@@ -2,70 +2,70 @@
 <?php
 if(isset($_POST['forminscription'])) {
 	$pseudo = htmlspecialchars($_POST['pseudo']);
-  $email = htmlspecialchars($_POST['email']);
-  $email2 = htmlspecialchars($_POST['email2']);
-  $mdp = htmlspecialchars($_POST['mdp']);
-  $mdp2 = htmlspecialchars($_POST['mdp2']);
+	$email = htmlspecialchars($_POST['email']);
+  	$email2 = htmlspecialchars($_POST['email2']);
+  	$mdp = htmlspecialchars($_POST['mdp']);
+  	$mdp2 = htmlspecialchars($_POST['mdp2']);
 	if(!empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['email2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
 		$valid = true;
     $pseudolength = strlen($pseudo);
     if($pseudolength <= 255) {
-			if($email == $email2) {
-				if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-					// On vérifit que le mail est disponible
-          $req_mail = $DB->query("SELECT email FROM users WHERE email = ?", array($email));
-          $req_mail = $req_mail->fetch();
-          if ($req_mail['email'] <> ""){
-						$valid = false;
-            $er_mail = "Ce email existe déjà";
-					}
-          if($valid != false) {
-						if($mdp == $mdp2) {
-							if($mdp < 8) {
-	              $mdp = crypt($mdp, '$6$rounds=5000$szgrzgerggegehrhhfshsh156s1@tfhs6h146-6GRS6G4¨^drfg4dg$');
-	              $date_inscription = date('Y-m-d H:i:s');
-	              // bin2hex(random_bytes($length))
-	              $token = bin2hex(random_bytes(12));
-	              // Exemples:
-	              // 39e9289a5b8328ecc4286da11076748716c41ec7fb94839a689f7dac5cdf5ba8bdc9a9acdc95b95245f80a00
-	              // On insert nos données dans la table utilisateur
-	              $DB->insert("INSERT INTO users(username, email, password, date_inscription, token) VALUES(?, ?, ?, ?, ?)", array($pseudo, $email, $mdp, $date_inscription, $token));
-	              $valide = "Votre compte a bien été créé ! <a href=\"connexion\">Me connecter</a>";
-	              //Envoi d'un email de confirmation de compte
-	              $req = $DB->query("SELECT * FROM users WHERE email = ?", array($email));
-	              $req = $req->fetch();
+		if($email == $email2) {
+			if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+				// On vérifit que le mail est disponible
+          		$req_mail = $DB->query("SELECT email FROM users WHERE email = ?", array($email));
+          		$req_mail = $req_mail->fetch();
+          		if ($req_mail['email'] <> ""){
+					$valid = false;
+				$er_mail = "Ce email existe déjà";
+				}
+				if($valid != false) {
+					if($mdp == $mdp2) {
+						if($mdp < 8) {
+	              			$mdp = crypt($mdp, '$6$rounds=5000$szgrzgerggegehrhhfshsh156s1@tfhs6h146-6GRS6G4¨^drfg4dg$');
+	              			$date_inscription = date('Y-m-d H:i:s');
+							// bin2hex(random_bytes($length))
+							$token = bin2hex(random_bytes(12));
+							// Exemples:
+							// 39e9289a5b8328ecc4286da11076748716c41ec7fb94839a689f7dac5cdf5ba8bdc9a9acdc95b95245f80a00
+							// On insert nos données dans la table utilisateur
+							$DB->insert("INSERT INTO users(username, email, password, date_inscription, token) VALUES(?, ?, ?, ?, ?)", array($pseudo, $email, $mdp, $date_inscription, $token));
+	              			$valide = "Votre compte a bien été créé ! <a href=\"connexion\">Me connecter</a>";
+	            			//Envoi d'un email de confirmation de compte
+	              			$req = $DB->query("SELECT * FROM users WHERE email = ?", array($email));
+	              			$req = $req->fetch();
 
-	              $mail_to = $req['email'];
-	              $subject = 'Prêts à raconter vos plus grandes aventures ?';
-	              $message = '<p>Bonjour ' . $req['username'] . ',</p><br>
+	              			$mail_to = $req['email'];
+	             			$subject = 'Prêts à raconter vos plus grandes aventures ?';
+	              			$message = '<p>Bonjour ' . $req['username'] . ',</p><br>
 									<p>Veuillez confirmer votre compte <a href="'. $url . '/conf?id=' . $req['id'] . '&token=' . $token . '">Valider</a><p>';
-	              $headers = array(
+	              			$headers = array(
 									'From' => 'Ceans de voyages.com <ceans@voyages.com>',
-	                'Reply-To' => 'ceans@voyages.com',
-	                'X-Mailer' => 'PHP/' . phpversion(),
-	                'Content-type' => 'text/html; charset=utf-8',
-	                'MIME-version' => '1.0',
-								);
-
-								mail($mail_to, $subject, $message, $headers);
-							} else {
-								$erreur = "Veuillez utiliser un mot de passe d'au moins 8 caractères";
-							}
+	                				'Reply-To' => 'ceans@voyages.com',
+	                				'X-Mailer' => 'PHP/' . phpversion(),
+	                				'Content-type' => 'text/html; charset=utf-8',
+	                				'MIME-version' => '1.0',
+							);
+							
+							mail($mail_to, $subject, $message, $headers);
 						} else {
-							$erreur = "Vos mots de passes ne correspondent pas !";
+							$erreur = "Veuillez utiliser un mot de passe d'au moins 8 caractères";
 						}
 					} else {
-						$erreur = "Adresse email déjà utilisée !";
+						$erreur = "Vos mots de passes ne correspondent pas !";
 					}
 				} else {
-					$erreur = "Votre adresse email n'est pas valide !";
+					$erreur = "Adresse email déjà utilisée !";
 				}
 			} else {
-				$erreur = "Vos adresses email ne correspondent pas !";
+				$erreur = "Votre adresse email n'est pas valide !";
 			}
 		} else {
-			$erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+			$erreur = "Vos adresses email ne correspondent pas !";
 		}
+	} else {
+		$erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+	}
 	} else {
 		$erreur = "Tous les champs doivent être complétés !";
 	}
